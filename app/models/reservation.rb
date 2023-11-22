e302e21728067528b65fdc1bf3ac6e421b31b415
菊川裕平 <yuh216@macbook-air.tailca2b4.ts.net>
2023-11-22 16:21:44 +0900

    バリデーション追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,21 @@
 class Reservation < ApplicationRecord
+    validates :checkin, presence: true
+    validates :checkout, presence: true
+    validates :person, presence: true, numericality: {greater_than: 0, message: " : 1人以上の人数を入れてください"}
+    validate :date_before_start
+    validate :start_end_check
+    belongs_to :user
+    belongs_to :room
+end
+
+def date_before_start
+    return if checkin.blank?
+    errors.add(:checkin, "は今日以降のものを選択してください") if checkin < Date.today
+end
+
+def start_end_check
+    if checkin != nil && checkout != nil
+    errors.add(:checkout, ": 開始日以前の日付は使用できません") unless
+    checkout > checkin
+    end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
-end
+    validates :room_name, presence: true
+    validates :room_content, presence: true
+    validates :room_price_day, presence: true
+    validates :room_address, presence: true
+    belongs_to :user
+    has_many :reservations
+  end
+  
