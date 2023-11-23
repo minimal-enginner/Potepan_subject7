bcfead9b196a5289cab0210eae5c5956d4b724cd
菊川裕平 <yuh216@macbook-air.local>
2023-11-23 17:16:39 +0900

    Usersモデルにimage,introductionカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_22_070225) do
+ActiveRecord::Schema.define(version: 2023_11_23_081341) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "checkin"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2023_11_22_070225) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "introduction"
     t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
